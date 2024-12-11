c2380bffafd7b8936a9c52ddff603309a4ec5582
LyMinhHau <hauly5639@gmail.com>
2024-12-11 18:00:22 +0700

    Hau

--- ChuyenDeASPNET/ChuyenDeASPNET/Controllers/HomeController.cs
+++ ChuyenDeASPNET/ChuyenDeASPNET/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChuyenDeASPNET.Context;
+using ChuyenDeASPNET.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,17 @@ namespace ChuyenDeASPNET.Controllers
 {
     public class HomeController : Controller
     {
+        ASPNETEntities objASPNETEntities = new ASPNETEntities();
         public ActionResult Index()
         {
-            return View();
+            HomeModel objHomeModel = new HomeModel();
+
+            objHomeModel.ListCategory = objASPNETEntities.Categories.ToList();
+            objHomeModel.ListProduct = objASPNETEntities.Products.ToList();
+            var lstProduct = objASPNETEntities.Products.ToList();
+            return View(objHomeModel);
         }
+
+
     }
 }
